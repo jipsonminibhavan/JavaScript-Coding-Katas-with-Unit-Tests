6178e433ca8dae854be01c044e7c6a9178eb2796
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2022-08-27 21:13:38 +0200

    feat:Solution

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,5 @@
-function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+
+
+      
 }
+
--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,42 @@
+// Add you solution here
+
+/*The sumApp class should have a 
+  property numbers which is an array*/
+
 class SumApp {
-  // Add you solution here
+  constructor() {
+    this.numbers = [];
+  }
+
+  /*The sumApp class should have a method addNumber(n) 
+  which will add the given number to the numbers array*/
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+  /* The sumApp class should have a method getSum()
+  which return the sum of all numbers 
+  added to the numbers array
+  -The getSum() method should return 0 
+  if no numbers have been added to the numbers array*/
+  getSum() {
+    //the sum is initialed to 0
+    let sum = 0;
+    //return the sum of all numbers
+    //add to the numbers array
+    //for (let number of this.numbers) {}
+
+    this.numbers.forEach((number) => {
+      sum += number;
+    });
+
+    console.log(sum);
+    return sum;
+  }
+
+  /*The sumApp class should have a method reset() 
+      which should delete all numbers previously 
+      added to the numbers array*/
+  reset() {
+    this.numbers = [];
+  }
 }
